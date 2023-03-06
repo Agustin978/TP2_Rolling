85a9606e171b4dff821f8e161fd6ad9620f06d53
Agustin978 <loboagus10@gmail.com>
2023-03-06 08:34:07 -0300

    Modificacion en los parametros del bucle para mostrar todos los caracteres de la frase correctamente

--- Desarrollo/js/ejercicio16.js
+++ Desarrollo/js/ejercicio16.js
@@ -4,7 +4,7 @@ do
     texto = prompt("Ingrese una palabra o frase: ");
 }while(!isNaN(texto) || texto.length < 4)
 
-for(let i = texto.length - 1; i > 0; i--)
+for(let i = texto.length - 1; i >= 0; i--)
 {
     document.write(texto[i]);
 }
